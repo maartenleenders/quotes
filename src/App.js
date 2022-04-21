1a5fafb57657286942458ef5c8b48802a2af7dc3
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-04-21 11:19:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Here be the best quotes</p>
       </header>
+	  <div>
+		  hello
+	  </div>
     </div>
   );
 }
