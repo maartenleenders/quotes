c6e083899deb14bcd995f59a63b8db44c963cd80
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-04-21 12:31:48 +0200

    Add form to main page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import AddForm from "./AddForm";
 import './App.css';
 
 function App() {
@@ -6,9 +7,17 @@ function App() {
       <header className="App-header">
         <p>Here be the best quotes</p>
       </header>
-	  <h1 className="text-3xl font-bold underline">
-      	Hello world!
-	  </h1>
+	  <div className="grid grid-cols-3 gap-2">
+		<div>
+			hoi
+		</div>
+		<div>
+			hoi2
+		</div>
+		<div className="text-left mt-6 border-gray-800 border-2 px-9 py-4 rounded-lg bg-white shadow">
+			<AddForm />
+		</div>
+	  </div>
     </div>
   );
 }
